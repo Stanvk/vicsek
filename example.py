0a043da47d0c9b8ee2be449ca8d4204064e9f1fa
Stan van Kraaij <stanvankraaij@hotmail.com>
2022-01-08 23:16:39 +0100

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import MatplotlibAnimator
 import Animator2D
 import Animator3D
 
-simulator = Vicsek.Vicsek(domainSize=(50,50,50), numberOfParticles=100)
+simulator = Vicsek.Vicsek(domainSize=(50,50), numberOfParticles=250)
 simulationData = simulator.simulate()
 
 """
@@ -14,12 +14,14 @@ animator = MatplotlibAnimator.MatplotlibAnimator(simulationData, (50,50,50))
 """
 Prepare the animator for a 2D representation.
 """
-preparedAnimator = animator.prepare(Animator3D.Animator3D())
+preparedAnimator = animator.prepare(Animator2D.Animator2D())
 
 """
 Execute and save the animation as an mp4.
-"""
+
+Optionally: (this requires ffmpeg)
 preparedAnimator.saveAnimation('vicsek2.mp4')
+"""
 
 """
 After saving the animation we can also show it directly.
